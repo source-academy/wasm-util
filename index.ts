896edc66e9e84aa007655484d95bc490b136c24d
jonasongg <jonasong2@gmail.com>
2026-02-23 17:43:23 +0800

    export types correctly

--- index.ts
+++ index.ts
@@ -7,7 +7,9 @@ export {
   instrToMethodMap,
   local,
   memory,
+  mut,
   wasm,
   type WatVisitor,
 } from "./src/builder.js";
 export { WatGenerator } from "./src/watGenerator.js";
+export * from "./src/types.js";
